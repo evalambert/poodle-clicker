358d894e5e9ed9a17569a39322a64ab88c51422d
evalambert <145194730+evalambert@users.noreply.github.com>
2023-10-23 17:56:04 +0200

    ajout code btn-autoClicker

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 (() => {
     let score = 0;
-    // Multiplicato
     let priceMultiplicator = 10; // Prix du multiplicateur début de partie 
     let multiplicator = 0; // Niveau du multiplicateur début de partie
 
     document.getElementById("score").innerHTML = score;
     document.getElementById("cost-multiplicator").innerHTML = priceMultiplicator;
 
-    //COOKIE-CLICKER
+    //COOKIE-CLICKER //////////////////////////////////////////////////////////////
     document.getElementById('btn-cookieClicker').addEventListener('click', function () {
         // A COMPLETER ! 
         // Condition pour faire en sorte que le multiplicator fonctionne sur le cookie.
@@ -22,7 +21,7 @@
         document.getElementById('score').textContent = score;
     });
 
-    // MULTIPLICATOR
+    // MULTIPLICATOR //////////////////////////////////////////////////////////////
     document.getElementById("btn-multiplicator").addEventListener("click", () => {
         if (score >= priceMultiplicator) {
             score -= priceMultiplicator;
@@ -40,39 +39,38 @@
         }
     });
 
+    // AUTOCLICK //////////////////////////////////////////////////////////////
 
+    let priceAutoClicker = 50
+    let autoClicker = 0 // 1 clic par seconde
 
-    // AUTOCLICK
-/* 
-    const scoreDisplay = document.querySelector('.score')
-    const btnAutoClick = document.getElementsByClassName('btn-autoClicker')
-    
-    let priceAutoClick = 50
-    let rateAutoClick = 1 // 1 clic par seconde
+    document.getElementById('cost-autoClicker').innerHTML = priceAutoClicker
 
-
-    function updateScore() {
-        scoreDisplay.textContent = score;
-        autoClickButton.textContent = `Clic automatique (Coût : ${autoClickCost} points)`;
+    function click() {
+        score += autoClicker
+        document.getElementById('score').textContent = score
     }
 
-    function autoClick() {
-        if (score >= priceAutoClick) {
-            score -= priceAutoClick
-            priceAutoClick *= 3 // Augmenter coût
-            rateAutoClick++
-            updateScore();
-            
+    document.getElementById('btn-autoClicker').addEventListener('click', () => {
+        if (score >= priceAutoClicker) {
+            score -= priceAutoClicker
+            priceAutoClicker *= 3 // Augmente le coût 
+            autoClicker++
+
             // Démarre le clic automatique
             setInterval(click, 1000) // 1000 ms = 1 sec.
+
+            // Mettez à jour le contenu des éléments
+            document.getElementById('score').innerHTML = score
+            document.getElementById('cost-autoClicker').innerHTML = priceAutoClicker
+        } else {
+            // Message d'erreur
+            alert("Vous n'avez pas assez de crédits")
         }
-    }
+    });
 
-    autoClickButton.addEventListener("click", autoClick);
 
-    updateScore();
-
- */
+    
 
 })();
 
