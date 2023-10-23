35c13e15b6ffb116b86e25f8431dfb1372993fb5
Thomas Syben <thomassyben07@gmail.com>
2023-10-23 15:11:01 +0200

    Add the multiplicator option

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 (() => {
-    let clicks = 100000;
-    let priceMultiplicator = 200;
+    let clicks = 0;
+    let priceMultiplicator = 10; // Prix du multiplicateur début de partie 
+    let multiplicator = 0; // Niveau du multiplicateur début de partie
 
     // Sélectionnez les éléments par leur ID, pas par leur classe
     document.getElementById("clicks").innerHTML = clicks;
     document.getElementById("costMultiplicator").innerHTML = priceMultiplicator;
 
+    //JS CODE COOKIE CLIKER
+    document.querySelector('.btn-cookieClicker').addEventListener('click', function () {
+        // A COMPLETER ! 
+        // Condition pour faire en sorte que le multiplicator fonctionne sur le cookie.
+
+        if(multiplicator>0){
+            clicks += 1*(multiplicator*2);
+        }
+        else{
+            clicks += 1;
+        }
+        // Réaliser les deux autres conditions en fonction de votre cas
+        document.querySelector('.score').textContent = clicks;
+    });
+
+    // Fonctionnement Multiplicator
     document.getElementById("multiplicator").addEventListener("click", () => {
         if (clicks >= priceMultiplicator) {
             clicks -= priceMultiplicator;
             priceMultiplicator *= 2;
-            console.log("clicks : " + clicks + ", price : " + priceMultiplicator);
+            multiplicator +=1;
+            // Historique de console
+            console.log("clicks : " + clicks + ", price : " + priceMultiplicator + " lvl : " + multiplicator);
 
             // Mettez à jour le contenu des éléments
             document.getElementById("clicks").innerHTML = clicks;
             document.getElementById("costMultiplicator").innerHTML = priceMultiplicator;
         } else {
+            // Message d'erreur
             alert("Vous n'avez pas assez de crédits");
         }
     });
