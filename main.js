75a4f82a8d62b5117ac21112b199def5388c2a7c
Thomas Syben <thomassyben07@gmail.com>
2023-10-23 14:35:23 +0200

    add multiplicator

--- main.js
+++ main.js
@@ -1,37 +1,21 @@
 (() => {
     let clicks = 100000;
-    let priceMultiplicator = 100;
-    let lvlMultiplicator = 0;
-    let priceAutoClick = 50;
-    let lvlAutoClick = 0;
-    let priceBoost = 500;
-    let lvlBoost = 0;
-    document.getElementById("target").innerHTML = clicks;
-    document.getElementById("price").innerHTML = price;
+    let priceMultiplicator = 200;
+
+    // Sélectionnez les éléments par leur ID, pas par leur classe
+    document.getElementById("clicks").innerHTML = clicks;
+    document.getElementById("costMultiplicator").innerHTML = priceMultiplicator;
 
     document.getElementById("multiplicator").addEventListener("click", () => {
-        if(clicks >= priceMultiplicator){
-            clicks -= price;
-            price *= 2;
-            lvl += 1;
-            document.getElementById("target").innerHTML = clicks;
-            document.getElementById("priceMultiplicator").innerHTML = price;
-            document.getElementById("lvlMultiplicator").innerHTML = lvl;
-        }
-        else{
-            alert("Vous n'avez pas assez de crédits");
-        }
-    });
-    document.getElementById("autoClick").addEventListener("click", () => {
-        if(clicks >= priceAutoClick){
-            clicks -= price;
-            price *= 2;
-            lvl += 1;
-            document.getElementById("target").innerHTML = clicks;
-            document.getElementById("priceMultiplicator").innerHTML = price;
-            document.getElementById("lvlMultiplicator").innerHTML = lvl;
-        }
-        else{
+        if (clicks >= priceMultiplicator) {
+            clicks -= priceMultiplicator;
+            priceMultiplicator *= 2;
+            console.log("clicks : " + clicks + ", price : " + priceMultiplicator);
+
+            // Mettez à jour le contenu des éléments
+            document.getElementById("clicks").innerHTML = clicks;
+            document.getElementById("costMultiplicator").innerHTML = priceMultiplicator;
+        } else {
             alert("Vous n'avez pas assez de crédits");
         }
     });
