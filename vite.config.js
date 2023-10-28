8d4472e4bbf8a67524ce5b3739be23b5b63d4f74
emanuele <emanuele971@icloud.com>
2023-10-28 19:19:28 +0200

    ema check :) fot vite deploying

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import fsExtra from 'fs-extra';
 
 export default defineConfig({
-  base: "/cookie-clicker/",
+  base: '/cookie-clicker/',
+
+  // ... altre configurazioni ...
+
+  build: {
+    // ... altre configurazioni di build ...
+
+    // Aggiungi questo passo post-build
+    afterBuild() {
+      // Copia i file da public a dist
+      fsExtra.copySync('public', 'dist');
+    },
+  },
 });
+
